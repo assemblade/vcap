d7596d6b3fae4abc30fc788552b3f2eed972bb81
shimizuh <hayato.shimizu@bskyb.com>
2012-10-18 15:27:27 +0100

    Updated router recipe to start up post installation

--- dev_setup/cookbooks/router/recipes/default.rb
+++ dev_setup/cookbooks/router/recipes/default.rb
@@ -4,12 +4,6 @@
 #
 # Copyright 2011, VMware
 #
-template node[:router][:config_file] do
-  path File.join(node[:deployment][:config_path], node[:router][:config_file])
-  source "router.yml.erb"
-  owner node[:deployment][:user]
-  mode 0644
-end
 
 
 bash "git clone router" do
@@ -23,3 +17,13 @@ bash "git clone router" do
 end
 
 cf_bundle_install(File.expand_path("router", node[:cloudfoundry][:home]))
+
+      
+template node[:router][:config_file] do
+   path File.join(node[:deployment][:config_path], node[:router][:config_file])
+   source "router.yml.erb"
+   owner node[:deployment][:user]
+   mode 0644
+  notifies :restart, "service[router]"
+end
+
