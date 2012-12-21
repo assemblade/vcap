d5b1a35ffc666ba72d6e95d5dfebd6107093b0c5
shimizuh <hayato.shimizu@bskyb.com>
2012-12-21 15:18:33 +0000

    Made dea base_dir into variable

--- dev_setup/cookbooks/dea/attributes/default.rb
+++ dev_setup/cookbooks/dea/attributes/default.rb
@@ -1,8 +1,10 @@
 default[:dea][:config_file] = "dea.yml"
 default[:dea][:local_route] = nil
 #default[:dea][:runtimes] = ["node04", "node06", "node08", "java", "java7", "erlang", "php", "python2"]
-default[:dea][:runtimes] = ["java", "java7", "php", "python2"]
+default[:dea][:runtimes] = ["java", "java7", "php", "python2", "ruby"]
 default[:dea][:logging] = 'debug'
 default[:dea][:secure] = false
 default[:dea][:multi_tenant] = true
 default[:dea][:enforce_ulimit] = false
+default[:dea][:base_dir] = /var/vcap.local/dea
+  
