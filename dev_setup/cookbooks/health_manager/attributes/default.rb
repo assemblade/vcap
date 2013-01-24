8057171c930ef30c79cd620a1b4b9aa7561f4bec
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-01-24 15:45:46 +0000

    Typo fix

--- dev_setup/cookbooks/health_manager/attributes/default.rb
+++ dev_setup/cookbooks/health_manager/attributes/default.rb
@@ -4,4 +4,4 @@ default[:health_manager][:database][:username] = node[:postgresql][:server_root_
 default[:health_manager][:database][:password] = node[:postgresql][:server_root_password]
 default[:health_manager][:local_route] = nil
 
-default[:health_manager][:github_url]} = "https://github.com/cloudfoundry/health_manager.git"
+default[:health_manager][:github_url] = "https://github.com/cloudfoundry/health_manager.git"
