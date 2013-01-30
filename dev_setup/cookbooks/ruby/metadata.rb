2d262a47bd711f80324cb34635bf67bcec70a99a
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-01-30 18:27:43 +0000

    Added recipe metadata

--- dev_setup/cookbooks/ruby/metadata.rb
+++ dev_setup/cookbooks/ruby/metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures Ruby"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0.0"
+
+recipe           "ruby", "Ruby 1.9.2"
+recipe           "ruby::ruby18", "Ruby 1.8"
+recipe           "ruby::ruby193", "Ruby 1.9.3"
