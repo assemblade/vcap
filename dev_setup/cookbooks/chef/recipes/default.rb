0ead28e0578f7d268af9461c9fec25596ee9c9e9
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-01-30 18:46:00 +0000

    Changed chef client cookbook parameter names

--- dev_setup/cookbooks/chef/recipes/default.rb
+++ dev_setup/cookbooks/chef/recipes/default.rb
@@ -1,6 +1,6 @@
 bash "Re-install chef with this rvm ruby" do
   code <<-EOH
-    if [ ! -e #{node[:ruby][:gempath]}/#{node[:ruby][:version]}/bin/chef-client ]
+    if [ ! -e #{node[:chef][:ruby_gempath]}/#{node[:chef][:ruby_version]}/bin/chef-client ]
     then
       source /usr/local/rvm/scripts/rvm
       gem install chef
