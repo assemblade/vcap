feb9f56e88e4e4d2a83c195a4e9d2bcb0f980862
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-02-05 10:40:30 +0000

    Updated ruby properties

--- dev_setup/cookbooks/ruby/attributes/default.rb
+++ dev_setup/cookbooks/ruby/attributes/default.rb
@@ -1,7 +1,7 @@
 include_attribute "deployment"
-default[:ruby][:version] = "1.9.2-p180"
-default[:ruby][:id] = "eyJzaWciOiJ5RDBXTXBNQTZ4RVRGTlo0SzBBR09tL0FkY0E9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMjFlMTIxMDA0ZTRlN2Q1MTFmNTUzMDUwMTlmMzMxMGZkMjQi%0AfQ==%0A"
-default[:ruby][:path] = File.join(node[:deployment][:home], "deploy", "rubies", "ruby-#{ruby[:version]}")
+default[:ruby192][:version] = "1.9.2-p180"
+default[:ruby192][:id] = "eyJzaWciOiJ5RDBXTXBNQTZ4RVRGTlo0SzBBR09tL0FkY0E9Iiwib2lkIjoi%0ANGU0ZTc4YmNhMjFlMTIxMDA0ZTRlN2Q1MTFmNTUzMDUwMTlmMzMxMGZkMjQi%0AfQ==%0A"
+default[:ruby192][:path] = File.join(node[:deployment][:home], "deploy", "rubies", "ruby-#{ruby[:version]}")
 default[:ruby][:checksums]["1.9.2-p180"] = "9027a5abaaadc2af85005ed74aeb628ce2326441874bf3d4f1a842663cde04f4"
 
 default[:rubygems][:version] = "1.8.24"
