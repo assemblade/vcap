4d0fe5d88eb9df6a95e78bae99b87253f4528b5f
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-03-28 17:45:43 +0000

    Java7 recipe fix

--- dev_setup/cookbooks/java7/recipes/default.rb
+++ dev_setup/cookbooks/java7/recipes/default.rb
@@ -12,8 +12,8 @@ when "ubuntu"
   pkg_path = File.join(node[:deployment][:setup_cache], "jre-7u4-linux-#{machine}.tar.gz")
   cf_remote_file pkg_path do
     owner node[:deployment][:user]
-    id node[:java7][:id]["#{machine}"]
-    checksum node[:java7][:checksum]["#{machine}"]
+    id node[:java7][:id][machine]
+    checksum node[:java7][:checksum][machine]
   end
 
   directory node[:java7][:java_home] do
