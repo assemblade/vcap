25645562602d7604b5149ee9f2a96fa5cccf72b8
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-03-28 15:38:55 +0000

    Fixed libpq-dev package name

--- dev_setup/cookbooks/essentials/recipes/default.rb
+++ dev_setup/cookbooks/essentials/recipes/default.rb
@@ -9,7 +9,7 @@
 
 %w{apt-utils build-essential libssl-dev libssl0.9.8 libreadline-dev libreadline6-dev libncurses5-dev
    libxml2 libxml2-dev libxslt1.1 libxslt1-dev git-core sqlite3 libsqlite3-ruby
-   libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev libpq5 libpq5-dev}.each do |p|
+   libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev libpq5 libpq-dev}.each do |p|
   package p do
     action [:install]
   end
