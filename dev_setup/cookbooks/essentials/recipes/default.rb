7fc6c7245b4d74d0253dd4e2783b0608a1d2a364
Hayato Shimizu <hayato.shimizu@bskyb.com>
2013-03-28 15:37:22 +0000

    Updated libpq installation

--- dev_setup/cookbooks/essentials/recipes/default.rb
+++ dev_setup/cookbooks/essentials/recipes/default.rb
@@ -9,7 +9,7 @@
 
 %w{apt-utils build-essential libssl-dev libssl0.9.8 libreadline-dev libreadline6-dev libncurses5-dev
    libxml2 libxml2-dev libxslt1.1 libxslt1-dev git-core sqlite3 libsqlite3-ruby
-   libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev}.each do |p|
+   libsqlite3-dev unzip zip ruby-dev libmysql-ruby libmysqlclient-dev libcurl4-openssl-dev libpq5 libpq5-dev}.each do |p|
   package p do
     action [:install]
   end
@@ -19,31 +19,3 @@ end
 directory "#{node[:deployment][:setup_cache]}" do
   action :create
 end
-
-machine =  node[:kernel][:machine]
-libpq_deb_path = File.join(node[:deployment][:setup_cache], "libpq5_9.2.deb")
-cf_remote_file libpq_deb_path do
-  owner node[:deployment][:user]
-  id node[:postgresql][:id][:libpq]["#{machine}"]
-  checksum node[:postgresql][:checksum][:libpq]["#{machine}"]
-end
-
-libpq_dev_deb_path = File.join(node[:deployment][:setup_cache], "libpq-dev_9.2.deb")
-cf_remote_file libpq_dev_deb_path do
-  owner node[:deployment][:user]
-  id node[:postgresql][:id][:libpq_dev]["#{machine}"]
-  checksum node[:postgresql][:checksum][:libpq_dev]["#{machine}"]
-end
-
-bash "Install libpq" do
-  code <<-EOH
-  dpkg -i #{libpq_deb_path}
-  EOH
-end
-
-bash "Install libpq-dev" do
-  code <<-EOH
-  dpkg -i #{libpq_dev_deb_path}
-  EOH
-end
-
