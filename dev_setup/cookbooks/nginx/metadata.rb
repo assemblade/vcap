1edfa2587751591a5cf85b07e7da1fd482593081
shimizuh <hayato.shimizu@bskyb.com>
2013-04-03 18:31:47 +0100

    Updated nginx version number

--- dev_setup/cookbooks/nginx/metadata.rb
+++ dev_setup/cookbooks/nginx/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "support@vmware.com"
 license          "Apache 2.0"
 description      "Installs/Configures nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "1.0.1"
